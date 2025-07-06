0aea78e15c86e0a5e06dcae85043c448c737a6c2
joshbowley <79362250+joshbowley@users.noreply.github.com>
2025-07-06 13:40:53 +0100

    Create sauce-regulator-v1.ino

---
+++ sauce-regulator/sauce-regulator-v1/sauce-regulator-v1.ino
@@ -0,0 +1,27 @@
+//fluid regulator prototype 1
+
+#include <Stepper.h>
+
+int stepsPerRevolution=2048;
+
+int motSpeed=2; //rpm
+
+Stepper myStepper(stepsPerRevolution,8,10,9,11);
+
+int dt=500;
+
+int baud=9600;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(baud);
+  myStepper.setSpeed(motSpeed);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  myStepper.step(-stepsPerRevolution/2);
+  delay(dt);
+  myStepper.step(stepsPerRevolution/2);
+  delay(dt);
+}
