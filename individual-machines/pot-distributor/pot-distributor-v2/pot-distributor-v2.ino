cc5d872b66a9b2cffef7e351fc033b71f38e36a4
joshbowley <79362250+joshbowley@users.noreply.github.com>
2025-07-30 17:06:15 +0100

    Create pot-distributor-v2.ino

---
+++ individual-machines/pot-distributor/pot-distributor-v2/pot-distributor-v2.ino
@@ -0,0 +1,36 @@
+//pot distributor v2
+
+//Requirements: 
+//motor moves one full negative rotation, then pause, and repeat.
+
+#include <AccelStepper.h>
+
+//create a stepper object - labelled M1
+AccelStepper M1(AccelStepper::FULL4WIRE, 8,9,10,11);
+
+const int stepsPerRevolution = 2048;  //for 28BYJ-48
+const float rpm = 5.0;
+const float motSpeed = rpm * stepsPerRevolution / 60.0; //steps per second
+const float motAccel = 100.0;  //steps per sec²
+const int dt = 2000;  //delay time between moves (ms)
+
+
+void setup() {
+  // put your setup code here, to run once:
+  M1.setMaxSpeed(motSpeed);
+  M1.setAcceleration(motAccel);
+  M1.setCurrentPosition(0); //set zero position
+  M1.moveTo(-stepsPerRevolution); //negative full turn
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (M1.distanceToGo() != 0 ){ 
+    M1.run();
+  }
+  else {
+    delay(dt); //pause
+    //go again and add another negative full rotation
+    M1.moveTo(M1.currentPosition() - stepsPerRevolution);
+  }
+}
